888de108aaffb427dd9cfa4dacf19803af324566
Niyati Rajukumar <niyati@example.com>
2026-06-07 14:19:55 +0530

    Update Roll category image

--- app/mobile/order/[cafeteriaId]/page.tsx
+++ app/mobile/order/[cafeteriaId]/page.tsx
@@ -38,7 +38,7 @@ const CATEGORY_IMAGES: { [key: string]: string } = {
   'Mandhi': 'https://qbvwcpjjattwebdzexni.supabase.co/storage/v1/object/public/menu-images/lit%20bites%20cafe/mandhi.jpg',
   'Combo': 'https://qbvwcpjjattwebdzexni.supabase.co/storage/v1/object/public/menu-images/lit%20bites%20cafe/combo.jpg',
   'Burger': 'https://qbvwcpjjattwebdzexni.supabase.co/storage/v1/object/public/menu-images/lit%20bites%20cafe/burger.jpg',
-  'Roll': 'https://images.unsplash.com/photo-1599599810694-b5ac4dd64761?w=600&h=400&fit=crop',
+  'Roll': 'https://qbvwcpjjattwebdzexni.supabase.co/storage/v1/object/public/menu-images/lit%20bites%20cafe/roll.jpg',
   'Alfaham': 'https://images.unsplash.com/photo-1598103442097-8b74394b95c6?w=600&h=400&fit=crop',
   'Fries': 'https://images.unsplash.com/photo-1585238341710-4b51926b4b13?w=600&h=400&fit=crop',
   'Drinks': 'https://images.unsplash.com/photo-1551182364-8a84ac993676?w=600&h=400&fit=crop',
